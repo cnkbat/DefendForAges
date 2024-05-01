884a51a7855e5888fec78da0a204d66460eace75
Dağhan Sever <daghansever@gmail.com>
2024-05-01 14:40:33 +0300

    New scene + enemy targeting
    EnemyTargeter foundation done

--- Assets/Scripts/Enemy/EnemyTargeter.cs
+++ Assets/Scripts/Enemy/EnemyTargeter.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyTargeter : MonoBehaviour
 {
     private Transform target;
-
-    void Start()
+    private float targetTimer;
+    private float closestDistance;
+    
+    private void Start()
     {
-
+        targetTimer = 2f;
+        closestDistance = -1;
     }
 
-    void Update()
+    // every two seconds, find the closest target
+    private void Update()
     {
-
+        if(targetTimer >= 2)
+        {
+            var targetList = FindObjectsOfType<MonoBehaviour>().OfType<ITargetable>();
+            
+            foreach(var targetable in targetList)
+            {
+                if(closestDistance == -1)
+                {
+                    SetTarget(targetable.target());
+                }
+                else
+                {
+                    if(closestDistance > Vector3.Distance(transform.position, targetable.target().position))
+                    {
+                        closestDistance = Vector3.Distance(transform.position, targetable.target().position);
+                        SetTarget(targetable.target());
+                    }
+                }
+            }
+        }
     }
-
-
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;

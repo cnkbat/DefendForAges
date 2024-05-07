6144f40014684ded76f693a5a47dbc5202950e89
cnkbat <cenkemirbat@gmail.com>
2024-05-07 23:49:52 +0300

    Update LoadableBase.cs

--- Assets/Scripts/Loadables/LoadableBase.cs
+++ Assets/Scripts/Loadables/LoadableBase.cs
@@ -3,24 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using System;
 
 public class LoadableBase : MonoBehaviour
 {
+    SaveManager saveManager;
+    PlayerStats playerStats;
     protected ObjectPooler objectPooler;
+    [SerializeField] private Transform moneyMovePos;
+
+    [Header("Costs")]
+    [SerializeField] protected List<int> costs;
+    protected int costIndex;
+    protected int currentCostLeftForUpgrade;
+
+    [Header("State")]
+    protected bool isFull;
+
+    [Header("Events")]
+    public Action OnLoadableFilled;
+
+    private void OnEnable() 
+    {
+
+        saveManager = SaveManager.instance;
+        saveManager.OnSaved += SaveData;
+
+    }
 
     protected virtual void Start()
     {
+
         objectPooler = ObjectPooler.instance;
+        playerStats = PlayerStats.instance;
+
+        UpdateCurrentCostLeft();
+
+        // Data load Save
+        
+        CheckIfFulled();
     }
 
     public virtual void Load()
     {
+        if (isFull) return;
 
+        currentCostLeftForUpgrade -= 1;
+        playerStats.DecrementMoney(1);
+
+        CheckIfFulled();
     }
 
-    public virtual void ResetLoadable()
+    protected void UpdateCurrentCostLeft()
     {
-        
+        currentCostLeftForUpgrade = costs[costIndex];
     }
+
+    protected void CheckIfFulled()
+    {
+
+        if (currentCostLeftForUpgrade <= 0)
+        {
+            isFull = true;
+            OnLoadableFilled?.Invoke();
+        }
+    }
+
+    protected virtual void SaveData()
+    {
+        // data save;
+    }
+    #region Getters & Setters
+    protected void SetCosts(List<int> newCosts)
+    {
+        costs = newCosts;
+    }
+
+    protected void SetCostIndex(int newCostIndex)
+    {
+        costIndex = newCostIndex;
+    }
+    #endregion
+
 }
 

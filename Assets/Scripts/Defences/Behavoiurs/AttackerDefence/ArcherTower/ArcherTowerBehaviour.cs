e953d3a26c67107d57c94d009c96e43622e66242
cnkbat <cenkemirbat@gmail.com>
2024-05-13 01:13:15 +0300

    arc tower fix

--- Assets/Scripts/Defences/Behavoiurs/AttackerDefence/ArcherTower/ArcherTowerBehaviour.cs
+++ Assets/Scripts/Defences/Behavoiurs/AttackerDefence/ArcherTower/ArcherTowerBehaviour.cs
@@ -17,11 +17,21 @@ public class ArcherTowerBehaviour : AttackerDefenceBehaviour
     protected override void OnEnable()
     {
         base.OnEnable();
+
+        for (int i = 0; i < attackerDefenceStat.GetWeapons().Count; i++)
+        {
+            OnRangedAttack += attackerDefenceStat.GetWeapons()[i].Attack;
+        }
     }
 
     protected override void OnDisable()
     {
         base.OnDisable();
+
+        for (int i = 0; i < attackerDefenceStat.GetWeapons().Count; i++)
+        {
+            OnRangedAttack -= attackerDefenceStat.GetWeapons()[i].Attack;
+        }
     }
 
     protected override void Start()
@@ -43,8 +53,9 @@ public class ArcherTowerBehaviour : AttackerDefenceBehaviour
     {
         base.Attack();
 
-        objectPooler.SpawnBulletFromPool(bulletTag, archerStickman.GetTipOfWeapon().position,
-            nearestEnemyFinder.GetNearestEnemy(), attackerDefenceStat.GetDamage());
+        if (!nearestEnemyFinder.GetNearestEnemy()) return;
+
+        OnRangedAttack?.Invoke(nearestEnemyFinder.GetNearestEnemy(), attackerDefenceStat.GetDamage(), false);
     }
 
     private void LookAtNearstEnemy(Transform closestEnemy)
@@ -63,7 +74,10 @@ public class ArcherTowerBehaviour : AttackerDefenceBehaviour
             worldAimTarget.y = archerStickman.transform.position.y;
             Vector3 aimDirection = (worldAimTarget - archerStickman.transform.position).normalized;
 
-            archerStickman.transform.forward = aimDirection;
+            for (int i = 0; i < attackerDefenceStat.GetWeapons().Count; i++)
+            {
+                attackerDefenceStat.GetWeapons()[i].transform.forward = aimDirection;
+            }
 
         }
 

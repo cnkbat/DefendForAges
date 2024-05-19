284d8e06b051b284bb426fc20570ddd625499dbd
cnkbat <cenkemirbat@gmail.com>
2024-05-19 16:07:13 +0300

    walla çarpınca merminin kırılması

---
+++ Assets/Scripts/Visual/EnvorimentSpecifier.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvorimentSpecifier : MonoBehaviour , IEnvironment
+{ 
+    // this script is for specifing walls, bricks etc.
+    // in order to bullet to break
+}

14202d16967bbe1c40fd1b29b130e4f4811db53f
cnkbat <cenkemirbat@gmail.com>
2024-05-24 02:57:16 +0300

    push

---
+++ Assets/Scripts/LevelDesignHelperPosition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelDesignHelperPosition : MonoBehaviour
+{
+    [SerializeField] float YAxisPos;
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            transform.position = new(transform.position.x, YAxisPos, transform.position.z);
+        }
+    }
+}

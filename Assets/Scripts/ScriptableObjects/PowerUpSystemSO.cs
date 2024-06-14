c42ead05f75ab06822087effe7c0aa59b33f51fe
cnkbat <cenkemirbat@gmail.com>
2024-06-15 01:07:30 +0300

    Update PowerUpSystemSO.cs

--- Assets/Scripts/ScriptableObjects/PowerUpSystemSO.cs
+++ Assets/Scripts/ScriptableObjects/PowerUpSystemSO.cs
@@ -8,25 +8,18 @@ public class PowerUpSystemSO : ScriptableObject
 {
     [Header("Attack Speed")]
     [SerializeField] private List<float> attackSpeedValues;
-    [SerializeField] private List<int> attackSpeedCosts;
 
     [Header("Range")]
     [SerializeField] private List<float> rangeValues;
-    [SerializeField] private List<int> rangeCosts;
 
     [Header("Movement Speed")]
     [SerializeField] private List<float> movementSpeedValues;
-    [SerializeField] private List<int> movementSpeedCosts;
-
 
     #region  Getters
 
     public List<float> GetAttackSpeedValues() { return attackSpeedValues; }
-    public List<int> GetAttackSpeedCosts() { return attackSpeedCosts; }
     public List<float> GetRangeValues() { return rangeValues; }
-    public List<int> GetRangeCosts() { return rangeCosts; }
     public List<float> GetMovementSpeedValues() { return movementSpeedValues; }
-    public List<int> GetMovementSpeedCosts() { return movementSpeedCosts; }
 
 
 

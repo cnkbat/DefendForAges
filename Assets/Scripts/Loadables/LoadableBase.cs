7e091270f7911378667b372e331897afae8d9d2d
Dağhan Sever <daghansever@gmail.com>
2024-06-20 10:31:51 +0300

    LoadableBase Repair Image

--- Assets/Scripts/Loadables/LoadableBase.cs
+++ Assets/Scripts/Loadables/LoadableBase.cs
@@ -28,6 +28,10 @@ public class LoadableBase : MonoBehaviour
     [SerializeField] private TMP_Text currentCostLeftForUpgradeText;
     [SerializeField] private TMP_Text repairText;
 
+    [Header("Texts")]
+    [SerializeField] private Image coinImage;
+    [SerializeField] private Image repairImage;
+
     [Header("Events")]
     public Action OnLoadableFilled;
     public Action OnRepairDone;
@@ -197,16 +201,25 @@ public class LoadableBase : MonoBehaviour
 
     public void SetIsRepairNeeded(bool newBool)
     {
-        // if(newBool){
-        //     currentCostLeftForUpgradeText.gameObject.transform.DOScale(0, 1f).SetEase(Ease.InElastic).OnComplete(() => currentCostLeftForUpgradeText.gameObject.SetActive(false));
-        //     repairText.gameObject.transform.DOScale(1, 1f).SetEase(Ease.InElastic).OnComplete(() => repairText.gameObject.SetActive(true));
-        // }
-        // else{
-        //     currentCostLeftForUpgradeText.gameObject.transform.DOScale(1, 1f).SetEase(Ease.InElastic).OnComplete(() => currentCostLeftForUpgradeText.gameObject.SetActive(true));
-        //     repairText.gameObject.transform.DOScale(0, 1f).SetEase(Ease.InElastic).OnComplete(() => repairText.gameObject.SetActive(false));
-        // }
-        currentCostLeftForUpgradeText.gameObject.SetActive(!newBool);
-        repairText.gameObject.SetActive(newBool);
+        // this doesnt seem to work, just setting active the repair text and image
+        if(newBool){
+            coinImage.gameObject.transform.DOScale(0, 1f).SetEase(Ease.InElastic).OnComplete(() => coinImage.gameObject.SetActive(false));
+            currentCostLeftForUpgradeText.gameObject.transform.DOScale(0, 1f).SetEase(Ease.InElastic).OnComplete(() => currentCostLeftForUpgradeText.gameObject.SetActive(false));
+            repairText.gameObject.SetActive(true);
+            repairText.gameObject.transform.DOScale(1, 1f).SetEase(Ease.InElastic);
+            repairImage.gameObject.SetActive(true);
+            repairImage.gameObject.transform.DOScale(3.3953f, 1f).SetEase(Ease.InElastic);
+        }
+        else{
+            repairImage.gameObject.transform.DOScale(0, 1f).SetEase(Ease.InElastic).OnComplete(() => repairImage.gameObject.SetActive(false));
+            repairText.gameObject.transform.DOScale(0, 1f).SetEase(Ease.InElastic).OnComplete(() => repairText.gameObject.SetActive(false));
+            currentCostLeftForUpgradeText.gameObject.SetActive(true);
+            currentCostLeftForUpgradeText.gameObject.transform.DOScale(1, 1f).SetEase(Ease.InElastic);
+            coinImage.gameObject.SetActive(true);
+            coinImage.gameObject.transform.DOScale(3.3953f, 1f).SetEase(Ease.InElastic);
+        }
+        // currentCostLeftForUpgradeText.gameObject.SetActive(!newBool);
+        // repairText.gameObject.SetActive(newBool);
 
         if (newBool)
         {
